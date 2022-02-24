eefe844b0bd2251d9a1ebabc3ded2e2fbaf3cc39
Constantine Oganesyants <53556592+OganesK@users.noreply.github.com>
2022-02-24 12:25:05 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -14,8 +14,8 @@ const whitelist = [
   'http://localhost:5000',
   'http://localhost:4000',
   'http://localhost:3000',
-  'https://www.elithys.space',
-  'http://www.elithys.space',
+  'http://www.elithys.space/graphql',
+  'https://www.elithys.space/graphql',
   'otherWhiteListURLs',
 ];
 
