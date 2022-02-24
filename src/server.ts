8597c924c11046a501fc700fe63ed622ab088aaf
Constantine Oganesyants <53556592+OganesK@users.noreply.github.com>
2022-02-24 12:23:59 +0300

    whitelist fix

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,8 @@ const whitelist = [
   'http://localhost:5000',
   'http://localhost:4000',
   'http://localhost:3000',
+  'https://www.elithys.space',
+  'http://www.elithys.space',
   'otherWhiteListURLs',
 ];
 
