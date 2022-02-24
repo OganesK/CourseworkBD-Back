807239b1f92dc87c63cf810c2b3c59a7231e21db
Constantine Oganesyants <53556592+OganesK@users.noreply.github.com>
2022-02-24 17:20:32 +0300

    Update index.ts

--- src/datasources/index.ts
+++ src/datasources/index.ts
@@ -1,6 +1,8 @@
 import { DataSources } from 'apollo-server-core/dist/graphqlOptions';
+import { UsersAPI } from './users';
 
 const getDataSources = () => ({
+  usersAPI: new UsersAPI(),
 });
 
 export interface ApolloDataSources extends DataSources<any> {
