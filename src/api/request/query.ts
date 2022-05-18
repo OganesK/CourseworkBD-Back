9f5dc5c0c5d571bbf5865b16367cd729cea4b6d2
Constantine Oganesyants <53556592+OganesK@users.noreply.github.com>
2022-05-18 16:24:06 +0300

    deploy

--- src/api/request/query.ts
+++ src/api/request/query.ts
@@ -1,9 +1,22 @@
-import { extendType } from "nexus";
+import { extendType, list } from "nexus";
+import { Context } from '../../graphql/context';
 
 export const RequestQuery = extendType({
   type: 'Query',
   definition (t) {
     t.crud.request();
     t.crud.requests();
+    t.field('getNotApprovedRequests', {
+      type: list('Request'),
+      args: {},
+      resolve: async (parent, args, ctx: Context) => {
+        const allProducts = await ctx.prisma.request.findMany({
+          where: {
+            approved: false
+          }
+        })
+        return allProducts;
+      }
+    })
   }
 })
